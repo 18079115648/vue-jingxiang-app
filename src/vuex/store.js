292ccf2c91e930a3c27bee79eb1afd643f1960d4
ybw <yibw@ydd100.cn>
2017-12-13 15:40:48 +0800

    location

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -7,9 +7,9 @@ import mutations from './mutations'
 Vue.use(Vuex)
 
 const state = {
-//	back_uri: 'http://localhost:8080/',
+	back_uri: 'http://' + window.location.host + '/',
 //  back_uri: 'http://mobile.jxdyf.cn/',//正式环境
-	back_uri: 'http://jx.shengshine.com/',//测试环境
+//	back_uri: 'http://jx.shengshine.com/',//测试环境
 	APP_ID: 'wx266523b29f3e06a2',
 	APP_SECRET: 'af6f9a2078557e7b8fc7480e0aebd2e8',
     loadingStatus: false
